800d7e0a5180034243dc09ee41348d4012f705fc
TomOwen93 <mr.tsowen@gmail.com>
2023-08-17 11:25:32 +0100

    initial commit with batch qrcodes

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,49 @@
-import { sum } from "./skeleton";
+// import QRCode from "qrcode";
+const QRCode = require("qrcode");
 
-// Call your function of interest, here, instead of sum.
-//You can use vscode's "Debug main.ts" launcher to debug, starting here.
+interface Channel {
+    url: string;
+    channelName: string;
+    fileName: string;
+}
+const urlArray = [
+    "https://www.youtube.com/@codeorg",
+    "https://www.youtube.com/@CoderbyteDevelopers",
+    "https://www.youtube.com/@Codesmith",
+    "https://www.youtube.com/@craigndave",
+    "https://www.youtube.com/@decomplexify",
+    "https://www.youtube.com/@Fireship",
+    "https://www.youtube.com/@freecodecamp",
+    "https://www.youtube.com/@funfunfunction",
+    "https://www.youtube.com/@KevinPowell",
+    "https://www.youtube.com/@NetNinja",
+    "https://www.youtube.com/@TraversyMedia",
+    "https://www.youtube.com/@WesBos",
+];
 
-const answer = sum(10, 200);
+const regexChannel = /.*@/;
 
-console.log({ answer });
+function createChannelObjects(array: string[]): Channel[] {
+    const channelObjects: Channel[] = array.map((url) => ({
+        url,
+        channelName: url.replace(regexChannel, ""),
+        fileName: `qrcode_${url.replace(regexChannel, "")}.png`,
+    }));
+    return channelObjects;
+}
+
+function createQRCode(channelObject: Channel) {
+    QRCode.toFile(
+        `./src/outputImages/${channelObject.fileName}`,
+        `${channelObject.url}`
+    );
+}
+
+function createBatchQRCodes(channelObjects: Channel[]) {
+    for (const element of channelObjects) {
+        createQRCode(element);
+    }
+}
+
+const channelObjects = createChannelObjects(urlArray);
+createBatchQRCodes(channelObjects);
